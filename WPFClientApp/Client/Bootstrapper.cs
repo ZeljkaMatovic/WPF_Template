ab103ab59fdaa5d09752ce576ab3eebca4e3bd13
Vuk Isic <visicev@gmail.com>
2020-07-06 21:58:11 +0200

    Initial

---
+++ WPFClientApp/Client/Bootstrapper.cs
@@ -0,0 +1,55 @@
+﻿using Caliburn.Micro;
+using Client.Services;
+using Client.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace Client
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        private readonly SimpleContainer container = new SimpleContainer();
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<MainWindowViewModel>();
+        }
+
+        protected override void Configure()
+        {
+            container.Instance(container);
+            container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<INotificationService, NotificationService>();
+
+            GetType().Assembly.GetTypes()
+            .Where(type => type.IsClass)
+            .Where(type => type.Name.EndsWith("ViewModel"))
+            .ToList()
+            .ForEach(viewModelType => container.RegisterPerRequest(
+                viewModelType, viewModelType.ToString(), viewModelType));
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            container.BuildUp(instance);
+        }
+
+    }
+}
---
+++ WPFClientApp/Client/Converters/BoolToStringConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Client.Converters
+{
+    public class BoolToStringConverter : IValueConverter
+    {
+        public string FalseValue { get; set; }
+        public string TrueValue { get; set; }
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? TrueValue : FalseValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
